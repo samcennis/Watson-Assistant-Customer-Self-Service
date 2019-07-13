17addf8d83e1b3213e8b548049896183e7138d62
samcennis <samennis@Sams-MacBook-Pro-3.local>
2019-07-13 00:36:56 -0700

    Added Cognos Analytics to page in IFrame
    Displays no screenshot if no Iframe configured

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,21 @@
+var appConfig = {
+  iFrameURL : (function(){return ('undefined' !== typeof cognosURL && cognosURL.length) ? cognosURL : false}())
+}
+
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
+  document.getElementById("myForm").style.display = "block";
+}
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
   
+window.onload = function() {
+  //If an IFrame for Cognos Analytics has not been configured, display a screenshot of a dashboard.
+  if (!appConfig.iFrameURL){
+    document.getElementById("mainContent").appendChild(document.createElement("img")).src = '/img/cognos-dashboard-sample.png';
+  }
+  else {
+    document.getElementById("mainContent").appendChild(document.createElement("iframe")).src = appConfig.iFrameURL;
+  }
+}
